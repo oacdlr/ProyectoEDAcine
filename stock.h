57d327ec13cf65323f7d6bee098fb419fe04f1bc
oacdlr <54248409+oacdlr@users.noreply.github.com>
2020-11-08 00:17:31 -0600

    Add files via upload

--- stock.h
+++ stock.h
@@ -1,12 +1,13 @@
 #ifndef STOCK_H_INCLUDED
 #define STOCK_H_INCLUDED
-#define ARCHIVO "Cartelera.csv"
+#define ARCHIVO "CarteleraDos.csv"
 #define MAX_TEXTO 30
-#define N 6
+#define N 9
 #include "sala.h"
 void leer_cartelera();
 void llenarArreglo(Pelicula *peliculas);
 void crearPelicula(Pelicula *peliculas);
-void listarCartelera(Pelicula peliculas);
+void listarPeli(Pelicula peliculas);
+void listarCartelera(Pelicula *peliculas);
 void liberarMemoria(Pelicula *peliculas);
 #endif 
